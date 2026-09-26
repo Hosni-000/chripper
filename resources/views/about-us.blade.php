93b7d0b5d4d78f3f88ecf2ba8af24ea9f04959ab
Hosni <hosnyali070@gmail.com>
2026-09-26 04:37:10 +0300

    add new layouts

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <x-slot:title>
+        About Us
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">About Us</h1>
+                    <p class="mt-4 text-base-content/60">Learn more about Chirper and our mission.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+Route::get('/about', function () {
+    return view('about-us');
+})->name('about');
